e35697c8792a6bb5d0ab7895dbfcc05555286675
airjordanlin-work <jlinwork0605@gmail.com>
2024-09-20 23:14:39 -0400

    Attempt 3 at getting vercel to work

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './index.public',
     }),
     new CopyPlugin({
       patterns: [
@@ -18,7 +18,7 @@ module.exports = merge(common, {
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
+        { from: '404.public', to: '404.public' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
